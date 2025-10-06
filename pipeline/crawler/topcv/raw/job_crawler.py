02752104ddc7d30ec2518d8ec68cb76155834a05
mnhtndev <tuannguyen.02042005@gmail.com>
2025-10-06 16:44:17 +0700

    job crawler version 3

--- pipeline/crawler/topcv/raw/job_crawler.py
+++ pipeline/crawler/topcv/raw/job_crawler.py
@@ -1,10 +1,12 @@
+import time
 import requests as rq
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
 
-
-header = ['job_id', 'title', 'income', 'location', 'experience', 'deadline', 'tags', 'last_mod']
+header = ['job_id', 'title', 'income', 'location', 'experience', 
+          'deadline', 'tags', 'job_description', 'candidate_requirements', 
+          'income_details', 'benefits', 'work_location', 'work_time', 'last_mod']
 job_id = 1
 last_mod = datetime.now ()
 url = 'https://www.topcv.vn/viec-lam/truong-phong-kinh-doanh-xe-o-to-vinfast/1802279.html'
@@ -50,7 +52,55 @@ tag_list = ''
 for tag in all_tags:
     value = tag.get_text (strip=True)
     tag_list += (value + ', ')
-    
+
+
+# job decriptions
+jd = soup.find ('div', class_ = 'job-description')
+jd_items = jd.find_all ('div', class_ = 'job-description__item')
+job_description, candidate_requirements, income_details, benefits, work_location, work_time = None, None, None, None, None, None
+
+#########           job_description
+job_desc_div = jd_items[0].find('div', class_='job-description__item--content')
+job_description = job_desc_div.get_text(separator="\n", strip=True) if job_desc_div else None
+
+#########           candidate_requirements
+req_div = jd_items[1].find('div', class_='job-description__item--content')
+if req_div:
+    requirements = [li.get_text(strip=True) for li in req_div.find_all("li")]
+    candidate_requirements = ''
+    for r in requirements:
+        candidate_requirements += (r + '\n')
+
+#########           income_details
+income_div = jd_items[2].find("div", class_="job-description__item--content")
+
+if income_div:
+    income_details = ''
+    details = [li.get_text(strip=True) for li in income_div.find_all("li")]
+    for d in details:
+        income_details += (d + '\n')
+
+#########           benefits
+benefits_div = jd_items[3].find("div", class_="job-description__item--content")
+if benefits_div:
+    benes = [li.get_text(strip=True) for li in benefits_div.find_all("li")]
+    benefits = ''
+    for b in benes:
+        benefits += (b + '\n')
+
+#########           work_location
+location_div = jd_items[4].find("div", class_="job-description__item--content")
+if location_div:
+    locations = [div.get_text(strip=True) for div in location_div.find_all("div")]
+    work_location = ''
+    for l in locations:
+        work_location += (l.replace ('- ', '') + '\n')
+
+
+#########           work_time
+work_time_div = jd_items[5].find("div", class_="job-description__item--content")
+if work_time_div:
+    work_time = work_time_div.get_text(strip=True)
 
 
 print("Job id:", job_id)
@@ -60,6 +110,12 @@ print("Location:", location_text)
 print("Experience:", exp_text)
 print("Deadline:", deadline_text)
 print("All tags:", tag_list)
+print("job_description:", job_description)
+print("candidate_requirements:", candidate_requirements)
+print("income_details:", income_details)
+print("benefits:", benefits)
+print("work_location:", work_location)
+print("work_time:", work_time)
 print("last_mod:", last_mod)
 
 # save to CSV
@@ -74,5 +130,11 @@ with open('data.csv', 'w', encoding='utf-8-sig', newline="") as f:
         "experience": exp_text,
         "deadline": deadline_text,
         "tags": tag_list,
+        "job_description": job_description,
+        "candidate_requirements": candidate_requirements,
+        "income_details": income_details,
+        "benefits": benefits,
+        "work_location": work_location,
+        "work_time": work_time,
         "last_mod": last_mod
     })
